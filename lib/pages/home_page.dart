49846e4666bd769b4413e037a1f419577f2bf226
UdaraNilupul <19it489@itum.mrt.ac.lk>
2023-03-19 14:42:54 +0530

    user_logout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser;
+
+  // sign out user
+  void signOutUser() async {
+    await FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('logged in'),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF4B5043),
+        actions: [
+          IconButton(
+            onPressed: signOutUser,
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          "Logged In",
+          style: TextStyle(fontSize: 20),
+        ),
       ),
     );
   }
